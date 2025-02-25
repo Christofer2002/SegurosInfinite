37ee8c57ea414e6452d82e33110d6bb7fb3a5e98
Gustavo Ordoñez <gustavoho2002@gmail.com>
2025-02-24 18:33:40 -0600

    controllerCreation

---
+++ SegurosInfinite/SegurosInfinite/Controllers/Login/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SegurosInfinite.Models;
+
+namespace SegurosInfinite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        [HttpPost]
+        [Consumes("application/json")]
+        public IActionResult registerClient([FromBody] Client client)
+        {
+            try
+            {
+                Service.Instance().registerClient(client);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                // Handle any exceptions or errors that may occur during registration
+
+                return StatusCode(500, "Error al registrar el cliente");
+            }
+        }
+    }
+
+}
+
