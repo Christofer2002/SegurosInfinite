dbbea2200b6d5167b8aef00b56cace6bf6368c3b
Christofer Daniel Chaves Pauth <42120690+Christofer2002@users.noreply.github.com>
2025-02-24 18:28:11 -0600

    Creation of models

---
+++ SegurosInfinite/SegurosInfinite/Models/Category/Category.cs
@@ -0,0 +1,32 @@
+﻿using SegurosInfinite.Models;
+using System.Collections.Generic;
+
+namespace SegurosInfinite.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public List<Coverage> Coverages { get; set; }
+
+        // Constructor without params
+        public Category()
+        {
+            Coverages = new List<Coverage>();
+        }
+
+        // Constructor with params
+        public Category(int id, string descripcion, List<Coverage> coberturas)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            Coverages = coberturas ?? new List<Coverage>();
+        }
+
+        public Category(string descripcion)
+        {
+            Descripcion = descripcion;
+            Coverages = new List<Coverage>();
+        }
+    }
+}
---
+++ SegurosInfinite/SegurosInfinite/Models/Coverage/Coverage.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using SegurosInfinite.Models;
+
+namespace SegurosInfinite.Models
+{
+    public class Coverage
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public double CostoMinimo { get; set; }
+        public double CostoPorcentual { get; set; }
+        public Category Category { get; set; }
+
+        // Constructor sin parámetros
+        public Coverage() { }
+
+        // Constructores con parámetros
+        public Coverage(int id, string descripcion, double costoMinimo, double costoPorcentual)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            CostoMinimo = costoMinimo;
+            CostoPorcentual = costoPorcentual;
+        }
+
+        public Coverage(string descripcion, double costoMinimo, double costoPorcentual)
+        {
+            Descripcion = descripcion;
+            CostoMinimo = costoMinimo;
+            CostoPorcentual = costoPorcentual;
+        }
+
+        public Coverage(int id, string descripcion, double costoMinimo, double costoPorcentual, Category categoria)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            CostoMinimo = costoMinimo;
+            CostoPorcentual = costoPorcentual;
+            Category = categoria;
+        }
+    }
+}
